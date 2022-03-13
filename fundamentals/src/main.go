e99c3a7e082a13f1138a8ed65ba68f7138d622fa
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2022-03-13 15:40:37 -0500

    go functions

--- fundamentals/src/main.go
+++ fundamentals/src/main.go
@@ -2,28 +2,37 @@ package main
 
 import "fmt"
 
+func normalFunction(message string) {
+	fmt.Println(message)
+}
+
+func tripleArgs(a int, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int {
+	return a * a
+}
+
+func doubleReturn(a int) (c, d int) {
+	return a, a * 2
+}
+
 func main() {
 
-	// fmt package
+	//? Functions
+	normalFunction("Hello Keko")
 
-	helloMessage := "Hello"
-	worldMessage := "World"
+	tripleArgs(149, 15, "Malia")
 
-	// Println
-	fmt.Println(helloMessage, worldMessage)
+	var x int = returnValue(2)
+	fmt.Println(x)
 
-	// Printf
-	nombre := "Keko kaka"
-	edad := 3
+	var value1, value2 int = doubleReturn(2)
+	fmt.Println(value1, value2)
 
-	fmt.Printf("%s tiene %d años\n", nombre, edad)
-
-	// Sprintf
-	message := fmt.Sprintf("%s tiene %d años", nombre, edad)
-
-	fmt.Println(message)
-
-	// Data type
-	fmt.Printf("helloMessage: %T", helloMessage)
+	// If we just want one value
+	var val1, _ int = doubleReturn(3)
+	fmt.Println(val1)
 
 }
