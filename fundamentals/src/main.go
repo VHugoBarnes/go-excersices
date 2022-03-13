25194e240cb7f4eafe9809b36ba6e5198db708b5
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2022-03-13 14:03:54 -0500

    Variables and types

--- fundamentals/src/main.go
+++ fundamentals/src/main.go
@@ -3,5 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+
+	// Constant
+	const name string = "keko"
+
+	// Variables
+	// Integers
+	// Not declared yet
+	base := 12
+	// Create and assign
+	var height float32 = 14
+	// Create
+	var weight float32
+
+	// Zero Value
+	var numero int
+	var cadena string
+	var booleano bool
+	var flotante32 float32
+	var flotante64 float64
+
+	// Excersice - square area
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+
+	fmt.Println(name)
+	fmt.Println("base: ", base)
+	fmt.Println("height: ", height)
+	fmt.Println("weight: ", weight)
+	fmt.Println("numero: ", numero)
+	fmt.Println("cadena: ", cadena)
+	fmt.Println("booleano: ", booleano)
+	fmt.Println("flotante32: ", flotante32)
+	fmt.Println("flotante64: ", flotante64)
+	fmt.Println("areaCuadrado: ", areaCuadrado)
+
 }
