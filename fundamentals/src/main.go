e0d9a98c049034f867091a05e40d5a7973c8b8b3
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2022-03-15 15:59:49 -0500

    Maps

--- fundamentals/src/main.go
+++ fundamentals/src/main.go
@@ -2,54 +2,24 @@ package main
 
 import "fmt"
 
-func isPalindromo(text string) string {
-	var textReverse string
-
-	for i := len(text) - 1; i >= 0; i-- {
-		textReverse += string(text[i])
-	}
-
-	if text == textReverse {
-		return "Es palindromo"
-	} else {
-		return "No es un palindromo"
-	}
-
-}
-
 func main() {
 
-	//? Arrays
-	var arr [4]int
+	//? Hash maps
+	m := make(map[string]int)
 
-	fmt.Println(arr, len(arr), cap(arr))
+	m["malia"] = 3
+	m["keko"] = 3
+	m["nicole"] = 22
 
-	//? Slices
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-
-	fmt.Println(slice, len(slice), cap(slice))
-
-	//? Métodos en el slice
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-
-	//? Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
-
-	//? Append new list
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
-
-	// Loop through arrays & slices
-	for _, value := range slice {
-		fmt.Println(value)
+	// loop map
+	for i, v := range m {
+		fmt.Println(i, v)
 	}
 
-	var palindromo string = "amor a roma"
+	// Find value
+	// with ok we check if the value exists
+	value, ok := m["malia"]
 
-	fmt.Println(palindromo, "es palindromo?", isPalindromo(palindromo))
+	fmt.Println(value, ok)
 
 }
