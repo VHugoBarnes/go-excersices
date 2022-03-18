8b4d7971909d292e391dbc178828c81a1db28716
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2022-03-17 21:45:30 -0500

    Len, cap, select

--- fundamentals/src/main.go
+++ fundamentals/src/main.go
@@ -2,20 +2,43 @@ package main
 
 import "fmt"
 
-func say(text string, c chan<- string) {
-
+func message(text string, c chan string) {
 	c <- text
-
 }
 
 func main() {
 
-	c := make(chan string, 1)
+	c := make(chan string, 2)
 
-	fmt.Println("Hello")
+	// Add str's to channel
+	c <- "Message 1"
+	c <- "Message 2"
 
-	go say("Bye", c)
+	// With len we check how many routines are in the channel
+	// With cap we check the maximmum capacity of the channel
+	fmt.Println(len(c), cap(c))
 
-	fmt.Print(<-c)
+	// Loop through channel with:
+	// range & close
+	close(c)
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("Hola", email1)
+	go message("Mundo", email2)
+
+	// Select
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email2", m2)
+		}
+	}
 
 }
